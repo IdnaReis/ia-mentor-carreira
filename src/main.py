30222099d1cc601ae09f9903dd3afe44bdd7379f
IdnaReis <idnadasilva@hotmail.com>
2026-03-23 18:44:24 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,20 @@
 
+🔹 3.  (ORQUESTRAÇÃO)
+
+from career import obter_habilidades_por_perfil
+from utils import formatar_lista
+
+
+def executar():
+    print("🚀 IA Mentor de Carreira\n")
+
+    perfil = input("Digite seu perfil (iniciante, estudante, transição): ")
+
+    habilidades = obter_habilidades_por_perfil(perfil)
+
+    print("\n💡 Sugestões de habilidades:\n")
+    print(formatar_lista(habilidades))
+
+
+if __name__ == "__main__":
+    executar()
